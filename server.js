cdf49b2fcad28398bc3940d938da8a46236897ff
Arun Sood <arsood@gmail.com>
2023-09-07 10:36:31 -0700

    Default port to 3000

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ app.get("/", (req, res) => {
   return res.status(200).send("Server response successful!");
 });
 
-const PORT = process.env.PORT || 80;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}...`);
